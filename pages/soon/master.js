ec63634483e0fe3242b3e7120acf75199b119d7e
ZackirM <metcalfz@msu.edu>
2021-04-16 09:53:08 -0400

    c

--- pages/soon/master.js
+++ pages/soon/master.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("April 10, 2021 00:00:00").getTime();
+var countDownDate = new Date("April 20, 2021 10:00:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
