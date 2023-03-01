295a1685446cbef5892f01139162a3fafcc34934
ZackirM <metcalfz@msu.edu>
2023-03-01 11:08:31 -0500

    c

--- construction/scripts/script.js
+++ construction/scripts/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feburary 20, 2023 12:00:00").getTime();
+var countDownDate = new Date("March 25, 2023 12:00:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
